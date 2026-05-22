c453b42e6c8771bbb32f4cf3d0e2a553e3cb58dc
blackbird-mx <blackbird_mx@proton.me>
2026-05-22 17:12:05 -0600

    add card selection

--- tonali-prototipe/src/carta.ts
+++ tonali-prototipe/src/carta.ts
@@ -1,12 +1,18 @@
 import * as Motor from 'excalibur';
 import { CartasSpriteSheet, FondosSpriteSheet } from './recursos';
 
+
 export class Carta extends Motor.Actor {
   private spriteIndex: number;
   private fondoIndex: number;
   public debug: boolean;
+  public selected: boolean = false;
+  private originalPos: Motor.Vector = Motor.vec(0, 0);
+  private fondoSprite: Motor.Sprite | null = null;
 
-  constructor(spriteIndex: number = 0, fondoIndex: number = 1, debug: boolean = false) {
+  constructor(spriteIndex: number = 0,
+              fondoIndex: number = 1,
+              debug: boolean = true) {
     super({
       name: 'Carta',
     });
@@ -16,11 +22,31 @@ export class Carta extends Motor.Actor {
   }
 
   override onInitialize(): void {
+    this.originalPos = this.pos.clone();
+
+    this.on('pointerdown', () => {
+      if (!this.selected) {
+        this.selected = true;
+        if (this.fondoSprite) {
+          this.fondoSprite.tint = Motor.Color.fromHex("#B15454B5");
+        }
+        this.actions.easeTo(this.originalPos.add(Motor.vec(0, -100)), 300, Motor.EasingFunctions.EaseInOutCubic);
+      } else {
+        this.selected = false;
+        if (this.fondoSprite) {
+          this.fondoSprite.tint = Motor.Color.Transparent;
+        }
+        this.actions.easeTo(this.originalPos, 300, Motor.EasingFunctions.EaseInOutCubic);
+      }
+    });
+
     // Background sprite from FondosSpriteSheet
-    const fondo = FondosSpriteSheet.getSprite(
+    const fondoOriginal = FondosSpriteSheet.getSprite(
       this.fondoIndex % 7,
       Math.floor(this.fondoIndex / 7)
     );
+    const fondo = fondoOriginal?.clone() ?? null;
+    this.fondoSprite = fondo;
 
     // Card face sprite from CartasSpriteSheet
     const sprite = CartasSpriteSheet.getSprite(
