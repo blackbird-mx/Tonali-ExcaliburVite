93e7c8774c7727973b5407e19807be99f2c28c3b
blackbird-mx <blackbird_mx@proton.me>
2026-07-17 22:21:01 -0600

    se agregan parametro de profundidad

--- tonali-prototipe/src/CajaRedondeada.ts
+++ tonali-prototipe/src/CajaRedondeada.ts
@@ -14,6 +14,7 @@ export interface CajaRedondeadaOptions {
   fontColor?: Motor.Color;
   fontSize?: number;
   fontFamily?: string;
+  z?: number;
 }
 
 /**
@@ -37,6 +38,7 @@ export class CajaRedondeada extends Motor.Actor {
       pos: Motor.vec(options.x, options.y),
       width: options.width,
       height: options.height,
+      z: options.z,
     });
 
     this.boxWidth = options.width;
